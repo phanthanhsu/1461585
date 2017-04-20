8a6f9d7f5ba556f940d67b23adabfe63cf7bda75
Phan Thanh Su <Phan Thanh Su>
2017-04-20 15:06:26 +0700

    Push lan 10, add partial view menu vao layout index

---
+++ DOANWEB2/ShopBUS/BUS.cs
@@ -0,0 +1,20 @@
+﻿using ShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBUS
+{
+    public class BUS
+    {
+        public static IEnumerable<LoaiSanPham> ListMenu()
+        {
+            using (var db = new ShopConnectionDB())
+            {
+                return db.Query<LoaiSanPham>("Select * From LoaiSanPham");
+            }
+        }
+    }
+}
