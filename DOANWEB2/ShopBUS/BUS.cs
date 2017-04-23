371c6f5d3a67acecb5c8b18e195f0b9bae3af030
Phan Thanh Su <Phan Thanh Su>
2017-04-23 20:27:18 +0700

    Push lan 15, Them trang admin va load danh sach san pham len admin

--- DOANWEB2/ShopBUS/BUS.cs
+++ DOANWEB2/ShopBUS/BUS.cs
@@ -9,6 +9,13 @@ namespace ShopBUS
 {
     public class BUS
     {
+        public static IEnumerable<SanPham> ListSP()
+        {
+            using (var db = new ShopConnectionDB())
+            {
+                return db.Query<SanPham>("Select * From SanPham");
+            }
+        }
         public static IEnumerable<LoaiSanPham> ListMenu()
         {
             using (var db = new ShopConnectionDB())
