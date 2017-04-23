b6060257bcda497cde45ade3fc59e3343cd97943
Phan Thanh Su <Phan Thanh Su>
2017-04-23 21:00:00 +0700

    Push lan 16, them giao dien trang loai san pham

--- DOANWEB2/DOANWEB2/Areas/Admin/Controllers/LoaiSanPhamController.cs
+++ DOANWEB2/DOANWEB2/Areas/Admin/Controllers/LoaiSanPhamController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopConnection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,9 @@ namespace DOANWEB2.Areas.Admin.Controllers
         // GET: Admin/LoaiSanPham
         public ActionResult Index()
         {
-            return View();
+            var db = new ShopConnectionDB();
+            IEnumerable<LoaiSanPham> dsLoaiSP = db.Query<LoaiSanPham>("Select * From LoaiSanPham");
+            return View(dsLoaiSP);
         }
 
         // GET: Admin/LoaiSanPham/Details/5
